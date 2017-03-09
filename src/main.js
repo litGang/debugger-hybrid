0a9d3f1ffce89f274585ffaebf0e07c28c8bdfd9
litGang <634469964@qq.com>
2017-03-10 00:32:21 +0800

    update

--- src/main.js
+++ src/main.js
@@ -7,26 +7,6 @@ import router from './router'
 Vue.config.productionTip = false;
 
 
-function setupWebViewJavascriptBridge(callback) {
-  console.log('!!!!')
-  if (window.WebViewJavascriptBridge) {
-    return callback(WebViewJavascriptBridge);
-  }
-  if (window.WVJBCallbacks) {
-    return window.WVJBCallbacks.push(callback);
-  }
-  window.WVJBCallbacks = [callback];
-  var WVJBIframe = document.createElement('iframe');
-  WVJBIframe.style.display = 'none';
-  WVJBIframe.src = 'https://__bridge_loaded__';
-  document.documentElement.appendChild(WVJBIframe);
-  setTimeout(function () {
-    document.documentElement.removeChild(WVJBIframe);
-  }, 0)
-}
-
-window.jsBridge = setupWebViewJavascriptBridge;
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
